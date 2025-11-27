c0a18f38420e01668605542cef45c66c5dc3bf07
Daksha-Belawadi <belawadidaksha@gmail.com>
2025-11-27 14:33:05 +0530

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,15 @@
+import sys
+if len(sys.argv)!=1
+temp=float(sys.argv[1])
+print(usage:python temp.py <temperature>)
+else:
+temp=25
+print("No input- using deafult temp")
+if temp < 15:
+  status = "cold"
+elif temp<= 30:
+  status = "normal"
+else:
+  status = "hot"
+
+print("Temperature Status",status)
